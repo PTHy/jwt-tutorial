291e59619154e5be3584741045e7f6659e68a959
PTHy <30455428+PTHy@users.noreply.github.com>
2018-04-06 23:47:30 +0900

    jwt token create
    token

--- routes/api/auth/auth.controller.js
+++ routes/api/auth/auth.controller.js
@@ -59,6 +59,41 @@ exports.register = (req, res) => {
     .catch(onError)
 }
 
+exports.check = (req,res) => {
+
+  //read the token from header or url
+  const token = req.header('x-access-token') || req.query.token
+
+  //token dose not exist
+  if(!token) {
+    return res.status(403).json({
+      success: false,
+      message: 'not logged in'
+    })
+  }
+
+  //create a promise that decodes the token
+  const p = new Promise(
+    (resolve, reject) => {
+      jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+        if(err) reject(err)
+        resolve(decoded)
+      })
+    }
+  )
+
+  //Error
+  const onError = (error) => {
+    res.status(403).json({
+      success: false,
+      message: error.message
+    })
+  }
+
+  //Promise
+  p.then(respond).catch(onError)
+}
+
 exports.login = (req,res) => {
   const {username, password} = req.body
   const secret = req.app.get('jwt-secret')
