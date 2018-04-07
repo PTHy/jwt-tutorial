56b9456b5abf845dc9a277ffc03fddd45e6e9f29
PTHy <30455428+PTHy@users.noreply.github.com>
2018-04-07 23:53:21 +0900

    jwt middleware take 1
    middleware

--- routes/api/auth/auth.controller.js
+++ routes/api/auth/auth.controller.js
@@ -58,42 +58,6 @@ exports.register = (req, res) => {
     .then(respond)
     .catch(onError)
 }
-
-exports.check = (req,res) => {
-
-  //read the token from header or url
-  const token = req.header('x-access-token') || req.query.token
-
-  //token dose not exist
-  if(!token) {
-    return res.status(403).json({
-      success: false,
-      message: 'not logged in'
-    })
-  }
-
-  //create a promise that decodes the token
-  const p = new Promise(
-    (resolve, reject) => {
-      jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
-        if(err) reject(err)
-        resolve(decoded)
-      })
-    }
-  )
-
-  //Error
-  const onError = (error) => {
-    res.status(403).json({
-      success: false,
-      message: error.message
-    })
-  }
-
-  //Promise
-  p.then(respond).catch(onError)
-}
-
 exports.login = (req,res) => {
   const {username, password} = req.body
   const secret = req.app.get('jwt-secret')
@@ -153,4 +117,45 @@ exports.login = (req,res) => {
   .then(check)
   .then(respond)
   .catch(onError)
+}
+exports.check = (req, res) => {
+    // read the token from header or url
+    const token = req.headers['x-access-token'] || req.query.token
+
+    // token does not exist
+    if(!token) {
+        return res.status(403).json({
+            success: false,
+            message: 'not logged in'
+        })
+    }
+
+    // create a promise that decodes the token
+    const p = new Promise(
+        (resolve, reject) => {
+            jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+                if(err) reject(err)
+                resolve(decoded)
+            })
+        }
+    )
+
+    // if token is valid, it will respond with its info
+    const respond = (token) => {
+        res.json({
+            success: true,
+            info: token
+        })
+    }
+
+    // if it has failed to verify, it will return an error message
+    const onError = (error) => {
+        res.status(403).json({
+            success: false,
+            message: error.message
+        })
+    }
+
+    // process the promise
+    p.then(respond).catch(onError)
 }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
-const auth = require('./auth')
+const controller = require('./auth.controller')
+const authMiddleware = require('../../../middlewares/auth')
 
-router.use('/auth', auth)
+router.post('/register', controller.register)
+router.post('/login', controller.login)
+
+router.use('/check', authMiddleware)
+router.get('/check', controller.check)
 
 module.exports = router
